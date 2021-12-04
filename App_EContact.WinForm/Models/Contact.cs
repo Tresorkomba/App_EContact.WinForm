5c373b7a059621d1606382255f24a4fca4548257
Trésor KAOMBA <tresorkaomba1@gmail.com>
2021-12-04 16:24:36 +0200

    DBContacts

--- App_EContact.WinForm/Models/Contact.cs
+++ App_EContact.WinForm/Models/Contact.cs
@@ -11,32 +11,35 @@ namespace App_EContact.WinForm.Models
         public int Id { get; set; }
         public string NomComplet { get; set; }
         public DateTime DateNaiss { get; set; }
+        public string LieuNaiss { get; set; }
         public string Email { get; set; }
         public string Sexe { get; set; }
         public string Tel { get; set; }
-        public string urlPhoto { get; set; }
+        public byte[] Photo { get; set; }
 
-        public Contact(string nomComplet, DateTime dateNaiss, string email, string sexe, string tel, string urlPhoto)
+        public Contact(string nomComplet, DateTime dateNaiss, string lieuNaiss, string email, string sexe, string tel,byte[] Photo)
         {
             NomComplet = nomComplet;
             DateNaiss = dateNaiss;
+            LieuNaiss = lieuNaiss;
             Email = email;
             Sexe = sexe;
             Tel = tel;
-            this.urlPhoto = urlPhoto;
+            this.Photo = Photo;
         }
         public Contact ()
         {
 
         }
-        public Contact(string nomComplet, DateTime dateNaiss, string email, string sexe, string tel, string urlPhoto)
+        public Contact(string nomComplet, DateTime dateNaiss, string lieuNaiss, string email, string sexe, string tel, string urlPhoto)
         {
             NomComplet = nomComplet;
             DateNaiss = dateNaiss;
+            LieuNaiss = lieuNaiss;
             Email = email;
             Sexe = sexe;
             Tel = tel;
-            this.urlPhoto = urlPhoto;
+            this.Photo = Photo;
         }
 
     }
---
+++ App_EContact.WinForm/Models/DBContact.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_EContact.WinForm.Models
+{
+    public static class DBContact
+    {
+        public static List<Contact> lstContacts;
+
+        // Ajouter un contact à la liste des contacts
+        public static int AddContact(Contact c)
+        {
+            if (lstContacts == null)
+            {
+                lstContacts = new List<Contact>();
+            }
+            c.Id = lstContacts.Any() ? lstContacts.Max(x => x.Id) + 1 : 100; // Lambda Expressions
+            lstContacts.Add(c);
+
+            return c.Id;
+        }
+        // Récuperer la liste des contacts
+        public static List<Contact> GetListContacs()
+        {
+            return lstContacts;
+        }
+        // Supprimer un contact
+        public static void DeleteContact(int IdContact)
+        {
+            int index = lstContacts.FindIndex(x => x.Id == IdContact);
+            lstContacts.RemoveAt(index);
+
+        }
+
+        // Modifier un contact
+        public static void UpdateContact(Contact c)
+        {
+            Contact rech =lstContacts.FirstOrDefault(x => x.Id == c.Id);
+            rech.NomComplet = c.NomComplet;
+            rech.DateNaiss = c.DateNaiss;
+            rech.LieuNaiss = c.LieuNaiss;
+            rech.Email = c.Email;
+            rech.Sexe = c.Sexe;
+            rech.Tel = c.Tel;
+            rech.Photo = c.Photo;
+        }
+        //Chercher par nom
+        public static List<Contact> SearchContact(string name)
+        {
+            List<Contact> res = lstContacts.Where(x => x.NomComplet.ToLower().Contains(name.ToLower())).ToList();
+            return res;
+        }
+}
+}
